97d5b6f2ab9695e750d3c0665b23c443eb2d868c
Chamara Somarathna <66479184+WhiteLeo9@users.noreply.github.com>
2025-10-13 17:39:41 -0400

    Update sph-streamlit.py

--- sph-streamlit.py
+++ sph-streamlit.py
@@ -8,6 +8,7 @@ import lammps_logfile
 from matplotlib.lines import Line2D
 from matplotlib.ticker import AutoMinorLocator
 
+plt.rcParams['font.family'] = 'serif'
 
 element_options = ['Au', 'Cu', 'Pt']
 temp_options = ['3000', '4000', '5000', '6000', '7000', '8000', '9000', '10000']
