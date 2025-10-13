7c882fe43f2acf4ab9a957b65084c1dcd496e81e
Chamara Somarathna <66479184+WhiteLeo9@users.noreply.github.com>
2025-10-13 15:10:01 -0400

    Update sph-streamlit.py

--- sph-streamlit.py
+++ sph-streamlit.py
@@ -7,15 +7,27 @@ import time
 import lammps_logfile
 
 element_options = ['Au', 'Cu', 'Pt']
-temp_options = [3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+temp_options = ['3000', '4000', '5000', '6000', '7000', '8000', '9000', '10000']
  
-st.title('Element select')
+st.title('Element selection')
 
-# Multi-select dropdown
-selected_comps = st.multiselect(
+# Element select dropdown
+selected_elements = st.multiselect(
     'Select one or more elements',
     element_options,
     default=['Au']
 )
-if selected_comps:
-    st.write(f'Selected elements: {", ".join(selected_comps)}')
+if selected_elements:
+    st.write(f'Selected elements: {", ".join(selected_elements)}')
+
+
+st.title('Temperature selection')
+
+# Temp select dropdown
+selected_temps = st.multiselect(
+    'Select one or more temperatures',
+    temp_options,
+    default=['3000']
+)
+if selected_temps:
+    st.write(f'Selected temperatures: {", ".join(selected_temps)}')
