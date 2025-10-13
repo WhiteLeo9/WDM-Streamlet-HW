babe8b77165afea28b73cad0c0fec676609c3d29
Chamara Somarathna <66479184+WhiteLeo9@users.noreply.github.com>
2025-10-13 17:14:32 -0400

    Update sph-streamlit.py

--- sph-streamlit.py
+++ sph-streamlit.py
@@ -8,6 +8,8 @@ import lammps_logfile
 
 element_options = ['Au', 'Cu', 'Pt']
 temp_options = ['3000', '4000', '5000', '6000', '7000', '8000', '9000', '10000']
+style_map = {'Au': '-', 'Cu': '--', 'Pt': ':'}
+color_map = {'3000': 'tab:blue', '4000': 'tab:orange', '5000': 'tab:green', '6000': 'tab:red', '7000': 'tab:purple', '8000': 'tab:brown', '9000': 'tab:pink', '10000': 'tab:olive'}
  
 st.title('Element selection')
 
@@ -40,8 +42,16 @@ for element in selected_elements:
         log = lammps_logfile.File(file_path)
         x = log.get("Time", run_num=1)
         y = log.get("c_Msd[4]", run_num=1)
-        ax.plot(x, y - y[0])
+        ax.plot(x, y - y[0], linestyle=style_map[element], color=color_map[temp])
 
+style_legend_elements = [Line2D([0], [0], color='black', lw=2, linestyle=style_map[el], label=el)
+                         for el in selected_elements]
+color_legend_elements = [Line2D([0], [0], color=color_map[t], lw=2, label=f'{t} K')
+                         for t in selected_temps]
+
+legend1 = ax.legend(handles=style_legend_elements, title="Element", loc='upper left')
+ax.add_artist(legend1)
+ax.legend(handles=color_legend_elements, title="Temperature", loc='lower right')
 
 ax.grid(False)
 st.pyplot(fig)
